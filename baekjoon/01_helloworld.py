7f1aeb1e37b6affd1bd2b03d03fe1a7b1ba7b185
EmjayAhn <emjay.data@gmail.com>
2019-01-16 09:46:05 +0900

    Feat: PEP8

--- baekjoon/01_helloworld.py
+++ baekjoon/01_helloworld.py
@@ -1,14 +1,16 @@
-# testcode
+# baekjoon/2557
 def solution():
-    return "Hello"
+    return "Hello World!"
 
+
+# testcode
 def test(func):
     results = [
         'Hello World!'
     ]
     for _ in results:
         assert func() == results[0]
-
     print("SUCCESS")
+
 
 test(solution)
