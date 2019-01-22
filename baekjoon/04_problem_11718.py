0448049e623e1495466453c5114ef7eee4fec6f4
EmjayAhn <emjay.data@gmail.com>
2019-01-22 11:24:10 +0900

    Feat: baekjoon_11718

---
+++ baekjoon/04_problem_11718.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/11718
+
+# inputs
+# Hello
+# Baekjoon
+# Online Judge
+
+# outputs
+# Hello
+# Baekjoon
+# Online Judge
+
+while True:
+    try:
+        print(input())
+    except:
+        break
