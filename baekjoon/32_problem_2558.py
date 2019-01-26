135bf5e36ccc4386320ffd3954e1781e17996692
EmjayAhn <emjay.data@gmail.com>
2019-01-26 17:52:56 +0900

    Feat: baekjoon_2558

---
+++ baekjoon/32_problem_2558.py
@@ -0,0 +1,8 @@
+# https://www.acmicpc.net/problem/2558
+# 백준 문제 2558
+import sys
+
+input1 = int(sys.stdin.readline().rstrip('\n'))
+input2 = int(sys.stdin.readline().rstrip('\n'))
+
+print(input1 + input2)
