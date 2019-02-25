dd8c971b07323cc2b603c59eff5b396786aa7c9e
EmjayAhn <emjay.data@gmail.com>
2019-02-25 10:05:36 +0900

    Feat: day6_central_limit_theorem3.py

---
+++ 06_hackerrank_statistics/day6_central_limit_theorem3.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/s10-the-central-limit-theorem-3/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+import sys
+import math
+
+samples = int(sys.stdin.readline().rstrip('\n'))
+mean = int(sys.stdin.readline().rstrip('\n'))
+std = int(sys.stdin.readline().rstrip('\n'))
+interval_prob = float(sys.stdin.readline().rstrip('\n'))
+z_value = float(sys.stdin.readline().rstrip('\n'))
+
+std = std / math.sqrt(samples)
+A = (-z_value) * std + mean
+B = z_value * std + mean
+
+sys.stdout.write(str(round(A, 2)) + '\n' + str(round(B, 2)))
+
+print(cumulative(515.68, mean, std) - cumulative(484.32, mean, std))
