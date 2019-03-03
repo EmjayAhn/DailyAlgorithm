83e0bc870b91ad29105981a1192eb03562124b40
EmjayAhn <emjay.data@gmail.com>
2019-03-03 22:28:48 +0900

    Feat: bst_level_order

---
+++ 04_30days_codechallenge/23_bst_level_order_traversal.py
@@ -0,0 +1,35 @@
+# https://www.hackerrank.com/challenges/30-binary-trees/tutorial
+
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        #Write your code here
+        cur = root
+        print(cur.data)
+        while cur:
+            
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
