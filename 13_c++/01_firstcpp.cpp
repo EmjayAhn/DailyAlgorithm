d42ea3ab3cd4fcc6c1eafb7452b5602f57c8609d
EmjayAhn <emjay.data@gmail.com>
2021-10-12 22:23:46 +0900

    Feat: Start studying C++

---
+++ 13_c++/01_firstcpp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main(void) {
+
+    //int : 자료형, Data Type
+    //i : 변수명
+    //자료형의 종류
+    int i = 0;
+    unsigned char c = 0;
+
+    c = 0;
+    c = 255;
+    c = 256;
+    c = -1;
+
+
+
+    cout << "HELLO\n";
+    return 0;
+
+}
