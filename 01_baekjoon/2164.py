34ccf7470d63a455c6090f89395ff67ffa052edb
emjayahn <emjay.data@gmail.com>
2024-07-03 22:58:00 +0900

    20240703

---
+++ 01_baekjoon/2164.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+def routine(input_list):
+    first = input_list.popleft()
+    second = input_list.popleft()
+    input_list.append(second)
+    
+N = int(input())
+input_list = deque([i for i in range(1, N+1)])
+
+while len(input_list) != 1:
+    routine(input_list)
+    
+print(input_list[0])
+    
