3876b91644785e91c13600948f0025bbdbd0a63b
emjayahn <emjay.data@gmail.com>
2024-07-10 23:07:41 +0900

    20240710

--- 01_baekjoon/2609.py
+++ 01_baekjoon/2609.py
@@ -1,7 +1,24 @@
+from math import sqrt
+
 A, B = map(int, input().split())
 
 def get_divisors(n):
 	s = set()
 	for i in range(1, int(sqrt(n))+1):
 		if n%i == 0:
+			s.add(i)
+			s.add(n // i)
+	return s
+
+def get_GCD(a, b):
+    set_a = get_divisors(a)
+    set_b = get_divisors(b)
+    
+    return max(set_a & set_b)
+
+def get_LCM(a, b):
+    return (a * b // get_GCD(a, b))
+
+print(get_GCD(A, B))
+print(get_LCM(A, B))
 			
