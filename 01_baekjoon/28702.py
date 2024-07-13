ab6782feb08a855cff74e6e39335bbfdf26ad629
emjayahn <emjay.data@gmail.com>
2024-07-14 01:08:08 +0900

    20240714

---
+++ 01_baekjoon/28702.py
@@ -0,0 +1,12 @@
+for i in range(3, 0, -1):
+    x = input()
+    if x not in ['Fizz', 'Buzz', 'FizzBuzz']:
+        n = int(x) + i
+if n%3==0 and n%5==0:
+    print('FizzBuzz')
+elif n%3==0:
+    print('Fizz')
+elif n%5==0:
+    print('Buzz')
+else:
+    print(n)
